1aa64dd781505f62eb8def53e77bead66861902f
bgrins <briangrinstead@gmail.com>
2011-08-26 21:35:40 -0500

    update content

--- public/js/app.js
+++ public/js/app.js
@@ -22,15 +22,23 @@ var slides = {
 		});
 		
 		$("#slides-collection").delegate("li", "click", function() {
-			//slides.activate()
+			slides.activate(slides.collection[0]);
 		});
+		
+		$("#slide-content").change(function() {
+			slides.active.content = $(this).val();
+		});
+	},
+	activate: function(slide) {
+		slides.active = slide;
+		$("#slide-content").val(slide.content);
 	},
 	add: function() {
 		slides.collection.push({ content: 'hi' });
 		
-		$("#slides-collection").html(slides.template({ slides: slides.collection })).sortable("destroy").sortable();
-		
-		
+		$("#slides-collection").
+			html(slides.template({ slides: slides.collection })).
+			sortable("destroy").sortable();
 	},
 	sync: function() {
 		alert(slides.template({ slides: slides.collection }))
