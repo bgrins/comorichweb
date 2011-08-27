09fc035d974fdd7ba5f2f715b84ddad04e0856c9
bgrins <briangrinstead@gmail.com>
2011-08-27 15:32:42 -0500

    update routes

---
+++ routes/site.js
@@ -0,0 +1,25 @@
+var fs = require("fs");
+var deck_repo = require("../models/deck.js");
+
+    module.exports = function(app){
+    
+    app.get("/dashboard", function(req, res) {
+    	if(!req.session.user) {
+    		res.redirect("/");
+    		return;
+    	}
+    	
+	    deck_repo.model.find({}, function(err, decks) {
+	        res.render("dashboard", { 
+	        	decks: decks,
+	        	layout: "site.ejs"
+	        });
+	    });
+    });
+        
+    app.get("/template", function(req, res) {
+        var content = req.param("content", "welcome to slideshow.js!");
+        res.render("templateeditor");
+    });
+
+};
