74394fc6ce4458fbb1b8b28b4356d677722e924c
bgrins <briangrinstead@gmail.com>
2011-08-27 14:44:43 -0500

    merging james and aarons work

--- routes/app.js
+++ routes/app.js
@@ -14,12 +14,17 @@ var deck_repo = require("../models/deck.js");
     });
     
     app.get("/dashboard", function(req, res) {
-    	deck_repo.model.find({}, function(err, decks) {
-    	    res.render("dashboard", { 
-    	    	decks: decks,
-    	    	layout: "site.ejs"
-    	    });
-    	});
+      	if(req.session.user){
+	    	deck_repo.model.find({}, function(err, decks) {
+	    	    res.render("dashboard", { 
+	    	    	decks: decks,
+	    	    	layout: "site.ejs"
+	    	    });
+	    	});
+    	}
+    	else {
+    		
+    	}
     });
         
     app.get("/template", function(req, res) {
@@ -27,16 +32,5 @@ var deck_repo = require("../models/deck.js");
         res.render("templateeditor");
     });
 
-    app.get('/your-slides', function(req, res){
-      var decks = [];
-      if(req.session.user){
-        deck_repo.model.findByUser(req.session.user, function(err, decks){
-          res.render('your-slides', {decks:decks});
-          console.log(decks)
-        });
-      }else{
-        res.render('your-slides', {decks:decks});
-      }
-    });
 };
 
